7a48da907feced085a35b5e06b304aed76c243af
Luca Ruiters <luca.ruiters@hva.nl>
2020-11-23 09:57:12 +0100

    Setup the QuizFragment code

--- app/src/main/java/tech/ru1t3rl/madlevel7example/QuizFragment.kt
+++ app/src/main/java/tech/ru1t3rl/madlevel7example/QuizFragment.kt
@@ -5,18 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import tech.ru1t3rl.madlevel7example.databinding.FragmentQuizBinding
 
 class QuizFragment : Fragment() {
+    private lateinit var binding: FragmentQuizBinding
+    private val viewModel: QuizViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+        binding = FragmentQuizBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        observerQuiz()
+    }
+
+    private fun observerQuiz() {
+        viewModel.quiz.observe(viewLifecycleOwner, {
+            val quiz = it
+            binding.tvQuizQuestion.text = quiz.question
+
+            binding.btConfirmAnswer.setOnClickListener {
+                if (viewModel.isAnwerCorrect(binding.etQuizAnswer.text.toString())) {
+                    Toast.makeText(context, "Your answer is correct!", Toast.LENGTH_LONG).show()
+                    findNavController().popBackStack()
+                } else {
+                    Toast.makeText(
+                        context,
+                        "You answer is incorrect! The correct answer was: ${quiz.answer}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        })
     }
 }
