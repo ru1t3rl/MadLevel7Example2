907e549d73ada40add1f1ce60673ed26608c7ef3
Luca Ruiters <luca.ruiters@hva.nl>
2020-11-23 09:52:48 +0100

    Setup the create QuizFragment code

--- app/src/main/java/tech/ru1t3rl/madlevel7example/CreateQuizFragment.kt
+++ app/src/main/java/tech/ru1t3rl/madlevel7example/CreateQuizFragment.kt
@@ -6,26 +6,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import tech.ru1t3rl.madlevel7example.databinding.FragmentCreateQuizBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class CreateQuizFragment : Fragment() {
+    private lateinit var binding: FragmentCreateQuizBinding
+    private val viewModel: QuizViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_quiz, container, false)
+        binding = FragmentCreateQuizBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.btCreateQuizConfirmAnswer.setOnClickListener {
+            viewModel.createQuiz(binding.etCreateQuizQuestion.text.toString(),
+                                 binding.etCreateQuizAnswer.text.toString())
         }
+
+        observeQuizCreation()
+    }
+
+    private fun observeQuizCreation() {
+        viewModel.createSuccess.observe(viewLifecycleOwner, {
+            Toast.makeText(activity, R.string.successfully_created_quiz, Toast.LENGTH_LONG).show()
+            findNavController().popBackStack()
+        })
+
+        viewModel.errorText.observe(viewLifecycleOwner, {
+            Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
+        })
     }
 }
